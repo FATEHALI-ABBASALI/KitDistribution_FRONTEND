12871510ec6b25987e7e15bc58e2729e6b389fcc
Fatehali Abbasali <156154922+FATEHALI-ABBASALI@users.noreply.github.com>
2026-04-24 20:10:05 +0530

    Update BeneficiaryReport.js

--- src/pages/admin/BeneficiaryReport.js
+++ src/pages/admin/BeneficiaryReport.js
@@ -80,7 +80,7 @@ export default function BeneficiaryReport() {
       const token = localStorage.getItem("token");
 
       const res = await fetch(
-        "http://localhost:8080/api/admin/beneficiary-report/pdf",
+        "https://kit-distribution-api-production-6268.up.railway.app/api/admin/beneficiary-report/pdf",
         {
           headers: {
             Authorization: "Bearer " + token
