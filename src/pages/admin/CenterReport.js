db52551d23c9c0708447e38043cca059c4c38d22
Fatehali Abbasali <156154922+FATEHALI-ABBASALI@users.noreply.github.com>
2026-04-24 20:10:57 +0530

    Update CenterReport.js

--- src/pages/admin/CenterReport.js
+++ src/pages/admin/CenterReport.js
@@ -73,7 +73,7 @@ export default function CenterReport() {
       const token = localStorage.getItem("token");
 
       const res = await fetch(
-        "http://localhost:8080/api/admin/center-wise-report/pdf",
+        "https://kit-distribution-api-production-6268.up.railway.app/api/admin/center-wise-report/pdf",
         {
           headers: {
             Authorization: "Bearer " + token
