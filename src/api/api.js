561b65f14f7defd772cef65e870c0d53c721bcb7
Fatehali Abbasali <156154922+FATEHALI-ABBASALI@users.noreply.github.com>
2026-04-24 20:07:17 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://kit-distribution-api-production-6268.up.railway.app";
 
 /* ================= TOKEN ================= */
 function getToken() {
